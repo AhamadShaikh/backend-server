3f1293f0807c294ca73550a17b2465b72228134d
AhamadShaikh <shaikhahamadrasul786@gmail.com>
2023-11-25 19:04:57 +0530

    backend blog api done

---
+++ middleware/auth.js
@@ -0,0 +1,29 @@
+
+const jwt = require("jsonwebtoken")
+
+const middleware = async (req, res, next) => {
+    const token = req.headers.authorization?.split(" ")[1]
+    try {
+
+        if (!token) {
+            return res.status(400).json("User Not Authorized")
+        }
+
+        const decode = jwt.verify(token, "ironman")
+
+        if (!decode) {
+            return res.status(400).json("User Not Authorized")
+        }
+
+        req.userId = decode.userId
+        req.email = decode.email
+        req.name = decode.name
+
+        next()
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = middleware
